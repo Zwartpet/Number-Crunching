72c424380ab6063735f919bbee15004237facd10
Tony Abidi <leaw@live.nl>
2014-04-03 12:57:39 +0200

    init dist

---
+++ dist/number-crunching/number-crunching/main.m
@@ -0,0 +1,24 @@
+//
+//  main.m
+//  number-crunching
+//
+//  Created by Tony Abidi on 03/04/14.
+//  Copyright (c) 2014 UVA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        printf("hello ");
+        
+    }
+    printf("\n");
+    return 0;
+}
+
