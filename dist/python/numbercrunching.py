e1981dbd85317d92b3a1bd108a395b7ef49a41c6
Rick <rick.slot@trifork.nl>
2014-05-08 13:10:25 +0200

    added python and start of report

---
+++ dist/python/numbercrunching.py
@@ -0,0 +1,83 @@
+import decimal
+import math
+import Queue
+
+class Node(object):
+	"""docstring for Node"""
+	def __init__(self, value, parent, operation):
+		self.value =  decimal.Decimal(value)
+		self.parent = parent
+		self.operation = operation
+
+class Tree(object):
+	"""docstring for Tree"""
+	def __init__(self, startNumber):
+		self.startNumber = decimal.Decimal(startNumber)
+		self.rootNode = Node(4, None, None)
+		self.queue = Queue.Queue()
+		self.queue.put(self.rootNode)
+		self.valuesToFind = list(range(1, 101))
+		self.alreadyFound = list()
+
+	def fillTree(self):
+		while not self.queue.empty():
+			node = self.queue.get()
+
+			if node.value in self.valuesToFind:
+				print "value: {0} queuesize: {1}".format(node.value, len(self.valuesToFind))
+				self.valuesToFind.remove(node.value)
+
+			# print self.queue.qsize()
+			# print node.value
+			# print node.operation
+			# print ""
+			if node.value not in self.alreadyFound:
+				if node.value._isinteger():
+					if node.value < 200000:
+						factorialNode = Node(math.factorial(node.value), node, "FACTORIAL")
+						node.factorialNode = factorialNode
+						self.queue.put(factorialNode)
+						#print factorialNode.value
+
+				if node.value >= 2:
+					sqrtNode = Node(node.value.sqrt(), node, "SQUAREROOT")
+					node.sqrtNode = sqrtNode		
+					self.queue.put(sqrtNode)
+					
+					#print sqrtNode.value
+				if not node.value._isinteger():
+					floorNode = Node(decimal.Decimal(math.floor(node.value)), node, "FLOOR")
+					node.floorNode = floorNode
+					self.queue.put(floorNode)
+			self.alreadyFound.append(node.value)
+
+
+def isWhole(x):
+	if(decimal.Decimal(x)%1 == 0):
+		return True
+	else:
+		return False
+
+
+
+print "running..."
+
+ 
+# a = decimal.Decimal()
+# a = math.factorial(a)
+# a = decimal.Decimal(a).sqrt()
+
+
+tree = Tree(4)
+tree.fillTree()
+
+#a = decimal.Decimal(1000.93)
+#a = decimal.Decimal(a).sqrt()
+#a = decimal.Decimal(math.floor(a))
+
+
+print a
+
+
+
+#print a
